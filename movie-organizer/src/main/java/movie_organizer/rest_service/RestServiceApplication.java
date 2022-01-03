57a838edd68f2a0d126d67c2b79241e9e46c4fab
jellyFishOne <jrenteria.camino@gmail.com>
2022-01-03 06:38:25 -0800

    initial commit

---
+++ movie-organizer/src/main/java/movie_organizer/rest_service/RestServiceApplication.java
@@ -0,0 +1,13 @@
+package movie_organizer.rest_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestServiceApplication.class, args);
+	}
+
+}
