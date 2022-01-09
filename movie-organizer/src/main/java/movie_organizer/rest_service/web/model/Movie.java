36d0a561d64bc87a7529cd78c5755bc8092cde42
jellyFishOne <jrenteria.camino@gmail.com>
2022-01-08 21:58:22 -0800

    Added validation to Movie model

--- movie-organizer/src/main/java/movie_organizer/rest_service/web/model/Movie.java
+++ movie-organizer/src/main/java/movie_organizer/rest_service/web/model/Movie.java
@@ -3,11 +3,37 @@ package movie_organizer.rest_service.web.model;
 import java.time.LocalDate;
 import java.util.List;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+@Document(collection ="movies")
 public class Movie {
-	//Variables
+	@Id
+	@Null
+	private String  id;
+	
+	@NotBlank
 	private String title;
+	
+	@NotNull
 	private MovieGenreEnum gengre;
+	
+	@NotNull
 	private LocalDate releaseDate;
+	
 	private int runTimeInMinutes;
 	private MovieRatingEnum movieRating;
 	private List<Actor> starring;
@@ -16,5 +42,4 @@ public class Movie {
 	private List<Producer> producer;
 	private int myRating;
 	private String myReview;
-	
 }
