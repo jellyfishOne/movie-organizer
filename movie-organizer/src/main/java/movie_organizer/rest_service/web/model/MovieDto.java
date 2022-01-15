1975d43014c40e4716e08a63f2629bd1b7196110
jellyFishOne <jrenteria.camino@gmail.com>
2022-01-14 19:09:30 -0800

    Testing movie services save new movie

--- movie-organizer/src/main/java/movie_organizer/rest_service/web/model/MovieDto.java
+++ movie-organizer/src/main/java/movie_organizer/rest_service/web/model/MovieDto.java
@@ -20,42 +20,48 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+
+@Document(collection ="movies")
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 @Builder
-@Document(collection ="movies")
 public class MovieDto {
-	
+	@Id
+	@Null
 	private String  id;
 	
-
+	@NotBlank
 	private String title;
 	
-	/*
-	 * @NotNull private MovieGenreEnum gengre;
-	 * 
-	 * @NotNull private LocalDate releaseDate;
-	 * 
-	 * @Positive private int runTimeInMinutes;
-	 * 
-	 * @NotNull private MovieRatingEnum movieRating;
-	 * 
-	 * @NotEmpty private List<Actor> starring;
-	 * 
-	 * @NotEmpty private List<Director> director;
-	 * 
-	 * @NotEmpty private List<Screenwriter> screenwriter;
-	 * 
-	 * @NotEmpty private List<Producer> producer;
-	 * 
-	 * @PositiveOrZero
-	 * 
-	 * @Min(value = 0, message = "Rating should not be less than zero.")
-	 * 
-	 * @Max(value = 10, message = "Rating should not be greater than 10.") private
-	 * int myRating;
-	 * 
-	 * private String myReview;
-	 */
+	@NotNull
+	private MovieGenreEnum gengre;
+	
+	@NotNull
+	private LocalDate releaseDate;
+	
+	@Positive
+	private int runTimeInMinutes;
+	
+	@NotNull
+	private MovieRatingEnum movieRating;
+	
+	@NotEmpty
+	private List<Actor> starring;
+	
+	@NotEmpty
+	private List<Director> director;
+	
+	@NotEmpty
+	private List<Screenwriter> screenwriter;
+	
+	@NotEmpty
+	private List<Producer> producer;
+	
+	@PositiveOrZero
+	@Min(value = 0, message = "Rating should not be less than zero.")
+	@Max(value = 10, message = "Rating should not be greater than 10.")
+	private int myRating;
+	
+	private String myReview;
 }
--- movie-organizer/src/test/java/movie_organizer/rest_service/RestServiceApplicationTests.java
+++ movie-organizer/src/test/java/movie_organizer/rest_service/RestServiceApplicationTests.java
@@ -1,9 +1,12 @@
 package movie_organizer.rest_service;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,7 +14,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import movie_organizer.rest_service.web.model.Actor;
 import movie_organizer.rest_service.web.model.Director;
 import movie_organizer.rest_service.web.model.MovieDto;
-import movie_organizer.rest_service.web.model.MovieDto.MovieBuilder;
 import movie_organizer.rest_service.web.model.MovieGenreEnum;
 import movie_organizer.rest_service.web.model.MovieRatingEnum;
 import movie_organizer.rest_service.web.model.Producer;
@@ -24,27 +26,47 @@ class RestServiceApplicationTests {
 	@Autowired
 	private MovieCollectionServices movieServices;
 	private LocalDate dateNow = LocalDate.now();
-	private List<Actor> actors = new ArrayList<Actor>();
-	private List<Director> directors = new ArrayList<Director>();
-	private List<Screenwriter> screenwriters = new ArrayList<Screenwriter>();
-	private List<Producer> producer = new ArrayList<Producer>();
-	private MovieBuilder movie;
+	private List<Actor> actors;
+	private Actor actor;	
+	private List<Director> directors;
+	private Director director;
+	private List<Screenwriter> screenwriters;
+	private Screenwriter screenwriter;
+	private List<Producer> producers;
+	private Producer producer;
+	private MovieDto movie; 
 	
-	/*
-	 * private Movie movie = Movie.builder() .id("1") .title("The Batman")
-	 * .gengre(MovieGenreEnum.ACTION) .releaseDate(dateNow) .runTimeInMinutes(190)
-	 * .movieRating(MovieRatingEnum.R_RESTRICTED) .starring(actors)
-	 * .director(directors) .screenwriter(screenwriters) .producer(producer)
-	 * .myRating(4) .myReview("very good, recomend");
-	 */
-	
-	
+	@BeforeEach
+	void init(){
+		actors = new ArrayList<Actor>();
+		actor = Actor.builder()
+				 .firtName("firtName")
+				 .surName("SurName").build();
+		actors.add(actor);
+		
+		directors = new ArrayList<Director>();
+		screenwriters = new ArrayList<Screenwriter>();
+		producers = new ArrayList<Producer>();
+		
+		
+		
+		movie =  MovieDto.builder()
+				.title("The Batman")
+				.gengre(MovieGenreEnum.ACTION) 
+				.releaseDate(dateNow) 
+				.runTimeInMinutes(190)
+				.movieRating(MovieRatingEnum.R_RESTRICTED) 
+				.starring(actors)
+				.director(directors) 
+				.screenwriter(screenwriters) 
+				.producer(producers)
+				.myRating(4) .myReview("very good, recomend").build();
+	}
+	 
 	@Test
 	void savedNewMovie() {
-		 movie = MovieDto.builder()
-				  .id("1")
-				  .title("Test builder");
 		MovieDto savedMovie = movieServices.saveNewMovie(movie);
+		assertNotNull(savedMovie);
 	}
 	
 	
