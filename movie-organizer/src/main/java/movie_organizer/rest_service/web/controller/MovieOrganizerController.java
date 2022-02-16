418830522b090a679ac94365842f6e34173da67f
jellyFishOne <jrenteria.camino@gmail.com>
2022-02-16 08:00:59 -0800

    Added POST save new movie

--- movie-organizer/src/main/java/movie_organizer/rest_service/web/controller/MovieOrganizerController.java
+++ movie-organizer/src/main/java/movie_organizer/rest_service/web/controller/MovieOrganizerController.java
@@ -1,8 +1,15 @@
 package movie_organizer.rest_service.web.controller;
 
+import javax.validation.Valid;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import movie_organizer.rest_service.web.model.MovieDto;
 import movie_organizer.rest_service.web.services.MovieCollectionServices;
 
 @RestController
@@ -19,6 +26,10 @@ public class MovieOrganizerController {
 	 * TODO Document function to Java Standards
 	 */
 	@PostMapping(value="/save-movie")
+	public ResponseEntity<MovieDto> saveNewMovie(@Valid @RequestBody MovieDto movieDto){
+		MovieDto savedMovie = movieServices.saveNewMovie(movieDto);
+		return ResponseEntity.status(HttpStatus.OK).body(savedMovie);
+	}
 	
 
 }
